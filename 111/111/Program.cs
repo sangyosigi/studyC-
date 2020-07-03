8e69f4e657b7d8d2e341bad84719183fe91a8f6b
sangyosigi <sangsleah@gmail.com>
2020-07-03 14:28:33 +0900

    gfg

--- 111/111/Program.cs
+++ 111/111/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using static System.Console;
 namespace _111
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("ex: HelloApp.exe<이름>");
+                return;
+            }
+            Console.WriteLine($"Hello,{args[0]}");
         }
     }
 }
